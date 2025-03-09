c4c74349acca8c9b8d0f6c42a154f715bcca0c30
Qdaika22 <quyet.dinhxuan@hcmut.edu.vn>
2025-03-09 09:34:46 +0700

    test

--- controllers/admin/config_controller.js
+++ controllers/admin/config_controller.js
@@ -1,26 +1,35 @@
-const userModel = require("../../models/user_model.js")
-const {prefixAdmin} = require("../../configs/system.js")
+const userModel = require("../../models/user_model.js");
+const { prefixAdmin } = require("../../configs/system.js");
 
 // [GET] /admin/config
 module.exports.home = async (req, res) => {
-    const configInfo = await userModel.getConfig();
+  const configInfo = await userModel.getConfig();
 
-    res.render("./admin/pages/config.pug", {
-        configInfo: configInfo
-    })
-}
+  res.render("./admin/pages/config.pug", {
+    configInfo: configInfo,
+  });
+  // Newline
+};
 
 // [POST] /admin/config
 module.exports.updateConfig = async (req, res) => {
-    const providingDate = req.body.providingDate;
-    const paperPerMonth = req.body.paperPerMonth;
-    const pdf = req.body.pdf;
-    const docx = req.body.docx;
-    const pptx = req.body.pptx;
-    const png = req.body.png;
-    const jpg = req.body.jpg;
+  const providingDate = req.body.providingDate;
+  const paperPerMonth = req.body.paperPerMonth;
+  const pdf = req.body.pdf;
+  const docx = req.body.docx;
+  const pptx = req.body.pptx;
+  const png = req.body.png;
+  const jpg = req.body.jpg;
 
-    await userModel.updateConfig(providingDate, paperPerMonth, pdf, docx, pptx, png, jpg);
+  await userModel.updateConfig(
+    providingDate,
+    paperPerMonth,
+    pdf,
+    docx,
+    pptx,
+    png,
+    jpg
+  );
 
-    res.redirect(`${prefixAdmin}/config`)
-}
+  res.redirect(`${prefixAdmin}/config`);
+};
